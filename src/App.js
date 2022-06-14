fbfd62171b34a33745602fe929b43fc14d042bc1
macbook <12345>
2022-06-14 20:18:48 +0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import ProductPage from "./features/product_details/presentation/ProductPage";
 import {Route, Routes} from "react-router-dom";
 import Main from "./Main";
+import Footer from "./general/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-        <Routes>
-            <Route path="/" element={<Main/>} exact/>
-            <Route path="product/:id" element={<ProductPage />} exact />
-        </Routes>
-    </div>
+      <>
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Main/>} exact/>
+                <Route path="product/:id" element={<ProductPage />} exact />
+            </Routes>
+        </div>
+        <Footer/>
+      </>
   );
 }
 
