b92054f68f78ab6d560ed543ed3061869a344cb4
fly2004 <fly.fly2004@gmail.com>
2022-06-16 14:07:10 +0300

    my changes and attempts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import logo from './logo.svg';
 import './App.css';
-import ProductPage from "./features/product_details/presentation/ProductPage";
 import {Route, Routes} from "react-router-dom";
-import Main from "./Main";
+import MainPage from "./features/main_pages_detailes/presentation/MainPage";
+
 
 function App() {
-  return (
-    <div className="App">
-        <Routes>
-            <Route path="/" element={<Main/>} exact/>
-            <Route path="product/:id" element={<ProductPage />} exact />
-        </Routes>
-    </div>
-  );
+    return (
+
+
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<MainPage/>} exact/>
+            </Routes>
+        </div>
+
+    );
 }
 
 export default App;
